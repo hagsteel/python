e1956a31551d8dbdb74fc46df8dffe3a41f582f0
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-10-09 21:55:33 +0700

    Fix play again by adding lower()

--- projects/number_guessing/main.py
+++ projects/number_guessing/main.py
@@ -1,13 +1,11 @@
 import random
-import time
 
-play_again = 'Y'
-while play_again == 'Y':
+play_again = 'y'
+while play_again == 'y':
     random_num = random.randrange(0, 5)
     user_guess = input("Guess the number: ")
     if user_guess == str(random_num):
         print(f"You guess it right the number was {random_num}")
     else:
         print(f"You guess it wrong the number was {random_num}")
-        time.sleep(1)
-    play_again = input("Play again [Y/N]: ")
+    play_again = input("Play again [Y/N]: ").lower()
