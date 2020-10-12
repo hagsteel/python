b21d7eadc31a2bea3f3da7a683ddc27f97d38c50
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-10-12 13:55:04 +0700

    Bug: exit buttons doesn't work on pygame window
    Remove print

--- pygame/oop_space_invaders/space_invaders.py
+++ pygame/oop_space_invaders/space_invaders.py
@@ -1,5 +1,6 @@
 from pygame import *
 from os.path import abspath, dirname
+import sys
 
 BASE_PATH = abspath(dirname(__file__))
 FONT_PATH = BASE_PATH + "/fonts/"
@@ -9,14 +10,11 @@ SOUND_PATH = BASE_PATH + "/sounds/"
 SCREEN = display.set_mode((800, 600))
 IMG_NAMES = ["ship", "enemy1"]
 IMAGES = {name: image.load(IMAGE_PATH + "{}.png".format(name)) for name in IMG_NAMES}
-print(IMAGES)
 
 class Ship:
     def __init__(self):
         self.image = IMAGES['ship']
         self.rect = self.image.get_rect(topleft=(375, 540))
-
-print(Ship().rect)
 
 class SpaceInvaders:
     def __init__(self):
@@ -25,6 +23,14 @@ class SpaceInvaders:
         self.caption = display.set_caption("Space Invaders")
         self.screen = SCREEN
         self.background = image.load(IMAGE_PATH + "background.jpeg")
+
+    def should_exit(evt):
+        return evt.type == QUIT
+
+    def check_input(self):
+        for event in event.get():
+            if self.should_exit(event):
+                sys.exit()
 
 if __name__ == "__main__":
     while True:
