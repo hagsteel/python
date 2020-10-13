02ee3fbd968b49f2ab276a2957d9213052085862
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-10-13 15:17:36 +0700

    Bug: Can't draw the text and background image

--- pygame/new_space_invaders/space_invaders.py
+++ pygame/new_space_invaders/space_invaders.py
@@ -4,26 +4,33 @@ import sys
 
 pygame.init()
 
-BASE_PATH = abspath(dirname(__file__))
-FONT_PATH = BASE_PATH + "/fonts/"
-IMAGE_PATH = BASE_PATH + "/images/"
-SOUND_PATH = BASE_PATH + "/sounds/"
-FONT = FONT_PATH + "space_invaders.ttf"
+# Create screen
+screen = pygame.display.set_mode((800, 600))
 
+# Caption
+pygame.display.set_caption("Space Invaders")
+
+BASE_PATH = abspath(dirname(__file__))
+
+# Font
+FONT_PATH = BASE_PATH + "/fonts/"
+FONT = FONT_PATH + "space_invaders.ttf"
 TITLE_FONT = pygame.font.Font(FONT, 50)
 SUB_TITLE_FONT = pygame.font.Font(FONT, 25)
+
+# Image and sound
+IMAGE_PATH = BASE_PATH + "/images/"
+SOUND_PATH = BASE_PATH + "/sounds/"
 
 # Colors (R, G, B)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-
-screen = pygame.display.set_mode((800, 600))
 IMG_NAMES = ["ship", "enemy1"]
 IMAGES = {name: pygame.image.load(IMAGE_PATH + "{}.png".format(name)) for name in IMG_NAMES}
 
+
 def main_menu():
-    screen.fill(BLACK)
     
     BACKGROUND = pygame.image.load(IMAGE_PATH + "background.jpeg")
     titletext = TITLE_FONT.render("Space Invaders", True, WHITE)
@@ -34,14 +41,12 @@ def main_menu():
     screen.blit(titletext2, (201, 225))
 
 
-def text():
-    Text(FONT, 50)
-
 def main():
     clock = pygame.time.Clock()
-    clock.tick(60)
 
     while True:
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
