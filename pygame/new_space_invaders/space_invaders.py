1d18810035a8e55ab3b96e89e591c21874d718ee
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-10-15 05:24:26 +0700

    Space ship support holding keys to move

--- pygame/new_space_invaders/space_invaders.py
+++ pygame/new_space_invaders/space_invaders.py
@@ -1,11 +1,11 @@
-import pygame
-from os.path import abspath, dirname
 import sys
+from os.path import abspath, dirname
+import pygame
 
 pygame.init()
 
 # Create screen
-screen = pygame.display.set_mode((800, 600))
+SCREEN = pygame.display.set_mode((800, 600))
 
 # Caption
 pygame.display.set_caption("Space Invaders")
@@ -24,8 +24,8 @@ IMAGE_PATH = BASE_PATH + "/images/"
 SOUND_PATH = BASE_PATH + "/sounds/"
 
 # Icon
-icon = pygame.image.load(IMAGE_PATH + "ufo.png")
-pygame.display.set_icon(icon)
+ICON = pygame.image.load(IMAGE_PATH + "ufo.png")
+pygame.display.set_icon(ICON)
 
 # Background image
 BACKGROUND = pygame.image.load(IMAGE_PATH + "background.jpeg")
@@ -41,30 +41,30 @@ BLACK = (0, 0, 0)
 GREEN = (78, 255, 87)
 
 # Game variables
-clock = pygame.time.Clock()
-fps = 60
+CLOCK = pygame.time.Clock()
+FPS = 60
 
 
 def main_menu():
     while True:
-        clock.tick(fps)
+        CLOCK.tick(FPS)
 
-        TITLETEXT = TITLE_FONT.render("Space Invaders", True, WHITE)
-        TITLETEXT2 = SUB_TITLE_FONT.render("Press enter to continue", True, WHITE)
+        title_text = TITLE_FONT.render("Space Invaders", True, WHITE)
+        title_text2 = SUB_TITLE_FONT.render("Press enter to continue", True, WHITE)
 
         # Enemy1 image
         enemy1 = pygame.image.load(IMAGE_PATH + "enemy1.png")
         # Resize enemy1
         enemy1 = pygame.transform.scale(enemy1, (40, 40))
         # Enemy points
-        pointtext = SUB_TITLE_FONT.render("   =   10 pts", True, GREEN)
+        point_text = SUB_TITLE_FONT.render("   =   10 pts", True, GREEN)
 
         # Drawing with positions
-        screen.blit(BACKGROUND, (0, 0))
-        screen.blit(TITLETEXT, (164, 155))
-        screen.blit(TITLETEXT2, (201, 225))
-        screen.blit(enemy1, (318, 270))
-        screen.blit(pointtext, (368, 270))
+        SCREEN.blit(BACKGROUND, (0, 0))
+        SCREEN.blit(title_text, (164, 155))
+        SCREEN.blit(title_text2, (201, 225))
+        SCREEN.blit(enemy1, (318, 270))
+        SCREEN.blit(point_text, (368, 270))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -76,30 +76,33 @@ def main_menu():
         pygame.display.update()
 
 
+left_down = False
+right_down = False
+pygame.key.set_repeat(1, 10)
+
+
 def main():
-    global ship_rect, ship_speed
+    global ship_rect, ship_speed, left_down, right_down
 
     while True:
-        clock.tick(fps)
+        CLOCK.tick(FPS)
 
-        screen.fill(BLACK)
+        SCREEN.fill(BLACK)
 
         # Drawing with positions
-        screen.blit(BACKGROUND, (0, 0))
+        SCREEN.blit(BACKGROUND, (0, 0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT and ship_rect.x > 10:
-                    ship_rect.x -= ship_speed
-                if event.key == pygame.K_RIGHT and ship_rect.x < 740:
-                    ship_rect.x += ship_speed
-            if event.type == pygame.K_UP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    ship_rect.x = 0
 
-        screen.blit(SHIP, ship_rect)
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                ship_rect.x -= ship_speed
+            if keys[pygame.K_RIGHT]:
+                ship_rect.x += ship_speed
+
+        SCREEN.blit(SHIP, ship_rect)
 
         pygame.display.update()
 
