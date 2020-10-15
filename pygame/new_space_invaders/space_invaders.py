b9d379355c5c73f87cd534d40570d0f0fc1e10d1
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-10-15 09:12:23 +0700

    Add live status on main screen

--- pygame/new_space_invaders/space_invaders.py
+++ pygame/new_space_invaders/space_invaders.py
@@ -18,6 +18,7 @@ FONT_PATH = BASE_PATH + "/fonts/"
 FONT = FONT_PATH + "space_invaders.ttf"
 TITLE_FONT = pygame.font.Font(FONT, 50)
 SUB_TITLE_FONT = pygame.font.Font(FONT, 25)
+MAIN_FONT = pygame.font.Font(FONT, 20)
 
 # Image and sound
 IMAGE_PATH = BASE_PATH + "/images/"
@@ -35,6 +36,9 @@ SHIP = pygame.image.load(IMAGE_PATH + "ship.png")
 ship_rect = SHIP.get_rect(topleft=(375, 540))
 ship_speed = 5
 
+# Enemy1
+ENEMY1 = pygame.image.load(IMAGE_PATH + "enemy1.png")
+
 # Colors (R, G, B)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -46,16 +50,16 @@ FPS = 60
 
 
 def main_menu():
+    global ENEMY1
+
     while True:
         CLOCK.tick(FPS)
 
         title_text = TITLE_FONT.render("Space Invaders", True, WHITE)
         title_text2 = SUB_TITLE_FONT.render("Press enter to continue", True, WHITE)
 
-        # Enemy1 image
-        enemy1 = pygame.image.load(IMAGE_PATH + "enemy1.png")
         # Resize enemy1
-        enemy1 = pygame.transform.scale(enemy1, (40, 40))
+        enemy1 = pygame.transform.scale(ENEMY1, (40, 40))
         # Enemy points
         point_text = SUB_TITLE_FONT.render("   =   10 pts", True, GREEN)
 
@@ -77,7 +81,7 @@ def main_menu():
 
 
 def main():
-    global ship_rect, ship_speed
+    global ship_rect, ship_speed, ENEMY1
 
     pygame.key.set_repeat(1, 10)
 
@@ -88,6 +92,18 @@ def main():
 
         # Drawing with positions
         SCREEN.blit(BACKGROUND, (0, 0))
+
+        # Lives
+        lives_text = MAIN_FONT.render("Lives", True, WHITE)
+
+        # Resize enemy1
+        enemy1 = pygame.transform.scale(ENEMY1, (25, 25))
+
+        # Drawing with positions
+        SCREEN.blit(lives_text, (640, 5))
+        SCREEN.blit(enemy1, (715, 3))
+        SCREEN.blit(enemy1, (742, 3))
+        SCREEN.blit(enemy1, (769, 3))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
