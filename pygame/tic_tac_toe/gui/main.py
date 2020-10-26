d53588f1b4d7a9c44b571185ec9562ec850aa5d2
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-10-26 14:26:07 +0700

    Bug: O image doesn't blit after the screen.blit(...)

--- pygame/tic_tac_toe/gui/main.py
+++ pygame/tic_tac_toe/gui/main.py
@@ -175,33 +175,29 @@ def ai():
             CURRENT_PLAYER_TURN = "X"
 
 
+first_time = True
+second_time = True
 def best_ai():
-    global CURRENT_PLAYER_TURN, BOARD
+    global CURRENT_PLAYER_TURN, BOARD, first_time, second_time
 
-    first_time = True
-
-    if first_time:
-        ai()
-        first_time = False
-
-    if BOARD[0][1] == 1 and BOARD[0][2] == 1:
+    if BOARD[0][1] == 1 and BOARD[0][2] == 1 and first_time:
         if BOARD[0][0] == 0:
             x = [50, 225, 400][0]
             y = [50, 225, 400][0]
             BOARD[0][0] = 2
             screen.blit(o_img, (x, y))
-        else:
-            ai()
+            first_time = False
 
-    if BOARD[1][1] == 1 and BOARD[1][2] == 1:
+    elif BOARD[1][1] == 1 and BOARD[1][2] == 1 and second_time:
         if BOARD[0][1] == 0:
             x = [50, 225, 400][0]
             y = [50, 225, 400][1]
-            BOARD[0][1] = 2
+            BOARD[1][0] = 2
             screen.blit(o_img, (x, y))
+            second_time = False
 
-        else:
-            ai()
+    else:
+        ai()
 
 
 def flip_ai_player():
