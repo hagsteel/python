b87a9f403c652599baec5a9eb2cac0abb70afc83
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-11-02 13:20:30 +0700

    Bug: Grouping enemies together

--- pygame/new_space_invaders/main.py
+++ pygame/new_space_invaders/main.py
@@ -1,4 +1,3 @@
-"""All the imports use in game"""
 import sys
 from os.path import abspath, dirname
 
@@ -6,62 +5,51 @@ import pygame
 
 pygame.init()
 
-# Create screen
 SCREEN = pygame.display.set_mode((800, 600))
 
-# Caption
 pygame.display.set_caption("Space Invaders")
 
-# Path
 BASE_PATH = abspath(dirname(__file__))
 
-# Font
 FONT_PATH = BASE_PATH + "/fonts/"
 FONT = FONT_PATH + "space_invaders.ttf"
 TITLE_FONT = pygame.font.Font(FONT, 50)
 SUB_TITLE_FONT = pygame.font.Font(FONT, 25)
 MAIN_FONT = pygame.font.Font(FONT, 20)
 
-# Image and sound
 IMAGE_PATH = BASE_PATH + "/images/"
 SOUND_PATH = BASE_PATH + "/sounds/"
 
-# Icon
 ICON = pygame.image.load(IMAGE_PATH + "ufo.png")
 pygame.display.set_icon(ICON)
 
-# Background image
 BACKGROUND = pygame.image.load(IMAGE_PATH + "background.jpeg")
 
-# Ship
 SHIP = pygame.image.load(IMAGE_PATH + "ship.png")
 SHIP_RECT = SHIP.get_rect(topleft=(375, 540))
 SHIP_SPEED = 5
 
-# Enemy1
 NUM_OF_ENEMIES = 10
-ENEMY1_IMG = []
+ENEMY_IMG = []
 ENEMY1_RECT = []
 ENEMY2_RECT = []
 ENEMY_SPEED = []
 ENEMY_PUSH_DOWN = []
-X = 115
+X = 65
 
 for enemies in range(NUM_OF_ENEMIES):
     X += 50
-    ENEMY1 = pygame.image.load(IMAGE_PATH + "enemy3_1.png")
-    # Resize enemy1
-    ENEMY1_IMG.append(pygame.transform.scale(ENEMY1, (40, 40)))
-    ENEMY1_RECT.append(ENEMY1_IMG[enemies].get_rect(topleft=(X, 60)))
-    ENEMY2_RECT.append(ENEMY1_IMG[enemies].get_rect(topleft=(X, 100)))
+    ENEMY = pygame.image.load(IMAGE_PATH + "enemy3_1.png")
+    ENEMY_IMG.append(pygame.transform.scale(ENEMY, (40, 40)))
+    ENEMY1_RECT.append(ENEMY_IMG[enemies].get_rect(topleft=(X, 60)))
+    ENEMY2_RECT.append(ENEMY_IMG[enemies].get_rect(topleft=(X, 100)))
     ENEMY_SPEED.append(2)
     ENEMY_PUSH_DOWN.append(40)
 
-# Bullet
 BULLET = pygame.image.load(IMAGE_PATH + "laser.png")
 BULLET_STATE = "Ready"
 BULLET_Y = 540
-BULLET_SPEED = 10
+BULLET_SPEED = 18
 
 # Colors (R, G, B)
 WHITE = (255, 255, 255)
@@ -81,7 +69,6 @@ def main_menu():
         title_text = TITLE_FONT.render("Space Invaders", True, WHITE)
         title_text2 = SUB_TITLE_FONT.render("Press enter to continue", True, WHITE)
 
-        # Enemy points
         point_text = SUB_TITLE_FONT.render("   =   10 pts", True, GREEN)
 
         # Drawing with positions
@@ -90,7 +77,7 @@ def main_menu():
         SCREEN.blit(title_text, (164, 155))
         SCREEN.blit(title_text2, (201, 225))
 
-        SCREEN.blit(ENEMY1_IMG[0], (318, 270))
+        SCREEN.blit(ENEMY_IMG[0], (318, 270))
         SCREEN.blit(point_text, (368, 270))
 
         for event in pygame.event.get():
@@ -113,8 +100,26 @@ def bunker():
 def draw_enemies():
     """Display the enemies"""
     # Enemy movement
+    all_enemy = []
     for i in range(NUM_OF_ENEMIES):
-        ENEMY1_RECT[i].x += ENEMY_SPEED[i]
+        all_enemy.append(ENEMY1_RECT[i].x)
+        all_enemy.append(ENEMY1_RECT[i].y)
+
+        ENEMY_SPEED = 2
+        all_enemy[-1] += ENEMY_SPEED
+        if all_enemy[-1] <= 1:
+            ENEMY_SPEED = 2
+            all_enemy[-1] += ENEMY_SPEED
+            # Push down in Y axis
+        elif all_enemy[-1] >= 755:
+            ENEMY_SPEED = -2
+            all_enemy[-1] += ENEMY_SPEED
+            # Push down in Y axis
+
+        SCREEN.blit(ENEMY_IMG[i], ENEMY1_RECT[i])
+        SCREEN.blit(ENEMY_IMG[i], ENEMY2_RECT[i])
+
+        """ENEMY1_RECT[i].x += ENEMY_SPEED[i]
         if ENEMY1_RECT[i].x <= 1:
             ENEMY_SPEED[i] = 2
             ENEMY1_RECT[i].y += ENEMY_PUSH_DOWN[i]
@@ -128,10 +133,7 @@ def draw_enemies():
             ENEMY2_RECT[i].y += ENEMY_PUSH_DOWN[i]
         elif ENEMY2_RECT[i].x >= 755:
             ENEMY_SPEED[i] = -2
-            ENEMY2_RECT[i].y += ENEMY_PUSH_DOWN[i]
-
-        SCREEN.blit(ENEMY1_IMG[i], ENEMY1_RECT[i])
-        SCREEN.blit(ENEMY1_IMG[i], ENEMY2_RECT[i])
+            ENEMY2_RECT[i].y += ENEMY_PUSH_DOWN[i]"""
 
 
 def bullet(spaceship_x, spaceship_y):
@@ -153,22 +155,21 @@ def main():
 
         SCREEN.fill(BLACK)
 
-        # Drawing with positions
         SCREEN.blit(BACKGROUND, (0, 0))
 
         # Lives
         lives_text = MAIN_FONT.render("Lives", True, WHITE)
-        # Resize enemy1 next to lives
-        tiny_enemy1 = pygame.transform.scale(ENEMY1, (25, 25))
+        # Resize enemy next to lives
+        tiny_enemy = pygame.transform.scale(ENEMY, (25, 25))
 
         score_text = MAIN_FONT.render("Score", True, WHITE)
         score_text_num = MAIN_FONT.render("0", True, GREEN)
 
         # Drawing with positions
         SCREEN.blit(lives_text, (640, 5))
-        SCREEN.blit(tiny_enemy1, (715, 3))
-        SCREEN.blit(tiny_enemy1, (742, 3))
-        SCREEN.blit(tiny_enemy1, (769, 3))
+        SCREEN.blit(tiny_enemy, (715, 3))
+        SCREEN.blit(tiny_enemy, (742, 3))
+        SCREEN.blit(tiny_enemy, (769, 3))
 
         SCREEN.blit(score_text, (5, 5))
         SCREEN.blit(score_text_num, (85, 5))
@@ -193,7 +194,6 @@ def main():
             BULLET_Y = 540
             BULLET_STATE = "Ready"
         if BULLET_STATE == "Fire":
-            bullet_x = SHIP_RECT.x
             bullet(bullet_x, BULLET_Y)
             BULLET_Y -= BULLET_SPEED
 
