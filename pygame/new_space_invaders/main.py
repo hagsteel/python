0de032e51e09a52ed69b366765bb8508a37198e5
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-11-09 10:32:32 +0700

    Refactor: Clean up code

--- pygame/new_space_invaders/main.py
+++ pygame/new_space_invaders/main.py
@@ -27,21 +27,22 @@ pygame.display.set_icon(ICON)
 BACKGROUND = pygame.image.load(IMAGE_PATH + "background.jpeg")
 
 SHIP = pygame.image.load(IMAGE_PATH + "ship.png")
-SHIP_RECT = SHIP.get_rect(topleft=(375, 540))
+ship_rect = SHIP.get_rect(topleft=(375, 540))
 SHIP_SPEED = 5
 
-explosion_sound = mixer.Sound(SOUND_PATH + "invaderkilled.wav")
-explosion_purple = pygame.image.load(IMAGE_PATH + "explosionpurple.png")
-explosion_cyan = pygame.image.load(IMAGE_PATH + "explosioncyan.png")
-explosion_green = pygame.image.load(IMAGE_PATH + "explosiongreen.png")
-explosion_purple = pygame.transform.scale(explosion_purple, (50, 40))
-explosion_cyan = pygame.transform.scale(explosion_cyan, (50, 40))
-explosion_green = pygame.transform.scale(explosion_green, (50, 40))
+EXPLOSION_SOUND = mixer.Sound(SOUND_PATH + "invaderkilled.wav")
+EXPLOSION_PURPLE = pygame.image.load(IMAGE_PATH + "explosionpurple.png")
+EXPLOSION_CYAN = pygame.image.load(IMAGE_PATH + "explosioncyan.png")
+EXPLOSION_GREEN = pygame.image.load(IMAGE_PATH + "explosiongreen.png")
+EXPLOSION_PURPLE = pygame.transform.scale(EXPLOSION_PURPLE, (50, 40))
+EXPLOSION_CYAN = pygame.transform.scale(EXPLOSION_CYAN, (50, 40))
+EXPLOSION_GREEN = pygame.transform.scale(EXPLOSION_GREEN, (50, 40))
 
 NUM_OF_ENEMIES = 10
-enemy_purple_img = []
-enemy_cyan_img = []
-enemy_green_img = []
+total_num_of_enemies = 50
+ENEMY_PURPLE_IMG = []
+ENEMY_CYAN_IMG = []
+ENEMY_GREEN_IMG = []
 enemy_purple_rect = []
 enemy1_cyan_rect = []
 enemy2_cyan_rect = []
@@ -54,23 +55,23 @@ enemy1_green_hit = []
 enemy2_green_hit = []
 ENEMY_SPEED = 1
 ENEMY_PUSH_DOWN = 10
-X = 105
+x = 105
 
 for enemy in range(NUM_OF_ENEMIES):
-    X += 50
+    x += 50
     ENEMY_PURPLE = pygame.image.load(IMAGE_PATH + "enemy1_2.png")
     ENEMY_CYAN = pygame.image.load(IMAGE_PATH + "enemy2_1.png")
     ENEMY_GREEN = pygame.image.load(IMAGE_PATH + "enemy3_1.png")
 
-    enemy_purple_img.append(pygame.transform.scale(ENEMY_PURPLE, (40, 40)))
-    enemy_cyan_img.append(pygame.transform.scale(ENEMY_CYAN, (40, 40)))
-    enemy_green_img.append(pygame.transform.scale(ENEMY_GREEN, (40, 40)))
+    ENEMY_PURPLE_IMG.append(pygame.transform.scale(ENEMY_PURPLE, (40, 40)))
+    ENEMY_CYAN_IMG.append(pygame.transform.scale(ENEMY_CYAN, (40, 40)))
+    ENEMY_GREEN_IMG.append(pygame.transform.scale(ENEMY_GREEN, (40, 40)))
 
-    enemy_purple_rect.append(enemy_purple_img[enemy].get_rect(topleft=(X, 80)))
-    enemy1_cyan_rect.append(enemy_cyan_img[enemy].get_rect(topleft=(X, 120)))
-    enemy2_cyan_rect.append(enemy_cyan_img[enemy].get_rect(topleft=(X, 160)))
-    enemy1_green_rect.append(enemy_green_img[enemy].get_rect(topleft=(X, 200)))
-    enemy2_green_rect.append(enemy_green_img[enemy].get_rect(topleft=(X, 240)))
+    enemy_purple_rect.append(ENEMY_PURPLE_IMG[enemy].get_rect(topleft=(x, 80)))
+    enemy1_cyan_rect.append(ENEMY_CYAN_IMG[enemy].get_rect(topleft=(x, 120)))
+    enemy2_cyan_rect.append(ENEMY_CYAN_IMG[enemy].get_rect(topleft=(x, 160)))
+    enemy1_green_rect.append(ENEMY_GREEN_IMG[enemy].get_rect(topleft=(x, 200)))
+    enemy2_green_rect.append(ENEMY_GREEN_IMG[enemy].get_rect(topleft=(x, 240)))
 
     enemy_purple_hit.append(False)
     enemy1_cyan_hit.append(False)
@@ -79,19 +80,19 @@ for enemy in range(NUM_OF_ENEMIES):
     enemy2_green_hit.append(False)
 
 BULLET = pygame.image.load(IMAGE_PATH + "laser.png")
-BULLET_STATE = "Ready"
-BULLET_Y = 540
-BULLET_X = 0
+bullet_state = "Ready"
+bullet_y = 540
+bullet_x = 0
 BULLET_SPEED = 18
-BULLET_RECT = BULLET.get_rect(topleft=(BULLET_X + 23, BULLET_Y))
+bullet_rect = BULLET.get_rect(topleft=(bullet_x + 23, bullet_y))
 
-bunkers = []
+BUNKERS = []
 
 score = 0
 
-mystery = pygame.image.load(IMAGE_PATH + "mystery.png")
-mystery_entered = mixer.Sound(SOUND_PATH + "mysteryentered.wav")
-mystery = pygame.transform.scale(mystery, (90, 40))
+MYSTERY_IMG = pygame.image.load(IMAGE_PATH + "mystery.png")
+MYSTERY_SOUND = mixer.Sound(SOUND_PATH + "mysteryentered.wav")
+MYSTERY_IMG = pygame.transform.scale(MYSTERY_IMG, (90, 40))
 MYSTERY_SPEED = 2
 
 WHITE = (255, 255, 255)
@@ -115,44 +116,44 @@ class MysteryState:
     def show_mystery(self):
         if self.mystery_is_visible:
             if self.mystery_entered_played:
-                mystery_entered.set_volume(0.03)
-                mystery_entered.play()
+                MYSTERY_SOUND.set_volume(0.03)
+                MYSTERY_SOUND.play()
                 self.mystery_entered_played = False
             if self.mystery_rect.x >= 800:
                 self.mystery_rect.x = 900
             else:
                 self.mystery_rect.x += MYSTERY_SPEED
-                SCREEN.blit(mystery, self.mystery_rect)
+                SCREEN.blit(MYSTERY_IMG, self.mystery_rect)
         if self.mystery_is_visible1:
             if self.mystery_entered_played1:
-                mystery_entered.set_volume(0.03)
-                mystery_entered.play()
+                MYSTERY_SOUND.set_volume(0.03)
+                MYSTERY_SOUND.play()
                 self.mystery_entered_played1 = False
             if self.mystery_rect1.x >= 800:
                 self.mystery_rect1.x = 900
             else:
                 self.mystery_rect1.x += MYSTERY_SPEED
-                SCREEN.blit(mystery, self.mystery_rect1)
+                SCREEN.blit(MYSTERY_IMG, self.mystery_rect1)
 
     def draw_mystery(self):
         self.mystery_is_visible = False
         self.mystery_is_visible1 = False
         for v in range(NUM_OF_ENEMIES):
-            if enemy_purple_rect[v].y >= 110 and enemy_purple_rect[v].y <= 300:
+            if 110 <= enemy_purple_rect[v].y <= 300:
                 self.mystery_is_visible = True
-            if enemy_purple_rect[v].y >= 150 and enemy_purple_rect[v].y <= 300:
+            if 150 <= enemy_purple_rect[v].y <= 300:
                 self.mystery_is_visible1 = True
 
         state.show_mystery()
 
-        if rect_intersect(BULLET_RECT, self.mystery_rect):
+        if rect_intersect(bullet_rect, self.mystery_rect):
             random_point_mystery(self.mystery_rect)
-        if rect_intersect(BULLET_RECT, self.mystery_rect1):
+        if rect_intersect(bullet_rect, self.mystery_rect1):
             random_point_mystery(self.mystery_rect1)
 
 
-state = MysteryState(mystery.get_rect(topleft=(-100, 40)),
-                     mystery.get_rect(topleft=(-100, 40)), True, True)
+state = MysteryState(MYSTERY_IMG.get_rect(topleft=(-100, 40)),
+                     MYSTERY_IMG.get_rect(topleft=(-100, 40)), True, True)
 
 
 def main_menu():
@@ -170,11 +171,11 @@ def main_menu():
         SCREEN.blit(BACKGROUND, (0, 0))
         SCREEN.blit(title_text, (164, 155))
         SCREEN.blit(title_text2, (201, 225))
-        SCREEN.blit(enemy_green_img[0], (300, 265))
+        SCREEN.blit(ENEMY_GREEN_IMG[0], (300, 265))
         SCREEN.blit(point_text_green, (350, 270))
-        SCREEN.blit(enemy_cyan_img[0], (300, 315))
+        SCREEN.blit(ENEMY_CYAN_IMG[0], (300, 315))
         SCREEN.blit(point_text_cyan, (350, 320))
-        SCREEN.blit(enemy_purple_img[0], (300, 365))
+        SCREEN.blit(ENEMY_PURPLE_IMG[0], (300, 365))
         SCREEN.blit(point_text_purple, (350, 370))
 
         for event in pygame.event.get():
@@ -191,12 +192,10 @@ def bunker():
     width = 100
     height = 50
     y_axis = 450
-    bunkers.append(
-        pygame.draw.rect(SCREEN, GREEN, (70, y_axis, width, height)))
-    bunkers.append(
-        pygame.draw.rect(SCREEN, GREEN, (350, y_axis, width, height)))
-    bunkers.append(
-        pygame.draw.rect(SCREEN, GREEN, (650, y_axis, width, height)))
+    draw_rect = pygame.draw.rect
+    BUNKERS.append(draw_rect(SCREEN, GREEN, (70, y_axis, width, height)))
+    BUNKERS.append(draw_rect(SCREEN, GREEN, (350, y_axis, width, height)))
+    BUNKERS.append(draw_rect(SCREEN, GREEN, (650, y_axis, width, height)))
 
 
 def draw_enemies():
@@ -249,16 +248,16 @@ def draw_enemies():
                 ENEMY_SPEED = -0.5
                 enemy2_green_rect[i].y += ENEMY_PUSH_DOWN
 
-        SCREEN.blit(enemy_purple_img[i], enemy_purple_rect[i])
-        SCREEN.blit(enemy_cyan_img[i], enemy1_cyan_rect[i])
-        SCREEN.blit(enemy_cyan_img[i], enemy2_cyan_rect[i])
-        SCREEN.blit(enemy_green_img[i], enemy1_green_rect[i])
-        SCREEN.blit(enemy_green_img[i], enemy2_green_rect[i])
+        SCREEN.blit(ENEMY_PURPLE_IMG[i], enemy_purple_rect[i])
+        SCREEN.blit(ENEMY_CYAN_IMG[i], enemy1_cyan_rect[i])
+        SCREEN.blit(ENEMY_CYAN_IMG[i], enemy2_cyan_rect[i])
+        SCREEN.blit(ENEMY_GREEN_IMG[i], enemy1_green_rect[i])
+        SCREEN.blit(ENEMY_GREEN_IMG[i], enemy2_green_rect[i])
 
 
 def bullet(spaceship_x, spaceship_y):
-    global BULLET_STATE
-    BULLET_STATE = "Fire"
+    global bullet_state
+    bullet_state = "Fire"
     SCREEN.blit(BULLET, (spaceship_x + 23, spaceship_y))
 
 
@@ -267,92 +266,89 @@ def rect_intersect(rect_zero, rect_one):
 
 
 def enemies_collision():
-    global BULLET_STATE, score, enemy_purple_rect, enemy1_cyan_rect
-    global enemy2_cyan_rect, enemy1_green_rect, enemy2_green_rect
-    global enemy_purple_hit, enemy1_cyan_hit, enemy2_cyan_hit, enemy1_green_hit, enemy2_green_hit
-    if BULLET_STATE == "Fire":
+    global total_num_of_enemies, bullet_state, score
+    global enemy_purple_rect, enemy1_cyan_rect, \
+        enemy2_cyan_rect, enemy1_green_rect, enemy2_green_rect
+    global enemy_purple_hit, enemy1_cyan_hit, enemy2_cyan_hit, \
+        enemy1_green_hit, enemy2_green_hit
+    if bullet_state == "Fire":
         for j in range(NUM_OF_ENEMIES):
-            if rect_intersect(BULLET_RECT, enemy_purple_rect[j]):
-                SCREEN.blit(explosion_purple, enemy_purple_rect[j])
+            if rect_intersect(bullet_rect, enemy_purple_rect[j]):
+                SCREEN.blit(EXPLOSION_PURPLE, enemy_purple_rect[j])
                 pygame.time.wait(20)
-                explosion_sound.set_volume(0.05)
-                explosion_sound.play()
+                EXPLOSION_SOUND.set_volume(0.05)
+                EXPLOSION_SOUND.play()
                 enemy_purple_rect[j].y = 600
-                BULLET_STATE = "Ready"
+                bullet_state = "Ready"
                 enemy_purple_hit[j] = True
                 score += 30
-            elif rect_intersect(BULLET_RECT, enemy1_cyan_rect[j]):
-                SCREEN.blit(explosion_cyan, enemy1_cyan_rect[j])
+                total_num_of_enemies -= 1
+            elif rect_intersect(bullet_rect, enemy1_cyan_rect[j]):
+                SCREEN.blit(EXPLOSION_CYAN, enemy1_cyan_rect[j])
                 pygame.time.wait(20)
-                explosion_sound.set_volume(0.05)
-                explosion_sound.play()
+                EXPLOSION_SOUND.set_volume(0.05)
+                EXPLOSION_SOUND.play()
                 enemy1_cyan_rect[j].y = 600
-                BULLET_STATE = "Ready"
+                bullet_state = "Ready"
                 enemy1_cyan_hit[j] = True
                 score += 20
-            elif rect_intersect(BULLET_RECT, enemy2_cyan_rect[j]):
-                SCREEN.blit(explosion_cyan, enemy2_cyan_rect[j])
+                total_num_of_enemies -= 1
+            elif rect_intersect(bullet_rect, enemy2_cyan_rect[j]):
+                SCREEN.blit(EXPLOSION_CYAN, enemy2_cyan_rect[j])
                 pygame.time.wait(20)
-                explosion_sound.set_volume(0.05)
-                explosion_sound.play()
+                EXPLOSION_SOUND.set_volume(0.05)
+                EXPLOSION_SOUND.play()
                 enemy2_cyan_rect[j].y = 600
-                BULLET_STATE = "Ready"
+                bullet_state = "Ready"
                 enemy2_cyan_hit[j] = True
                 score += 20
-            elif rect_intersect(BULLET_RECT, enemy1_green_rect[j]):
-                SCREEN.blit(explosion_green, enemy1_green_rect[j])
+                total_num_of_enemies -= 1
+            elif rect_intersect(bullet_rect, enemy1_green_rect[j]):
+                SCREEN.blit(EXPLOSION_GREEN, enemy1_green_rect[j])
                 pygame.time.wait(20)
-                explosion_sound.set_volume(0.05)
-                explosion_sound.play()
+                EXPLOSION_SOUND.set_volume(0.05)
+                EXPLOSION_SOUND.play()
                 enemy1_green_rect[j].y = 600
-                BULLET_STATE = "Ready"
+                bullet_state = "Ready"
                 enemy1_green_hit[j] = True
                 score += 10
-            elif rect_intersect(BULLET_RECT, enemy2_green_rect[j]):
-                SCREEN.blit(explosion_green, enemy2_green_rect[j])
+                total_num_of_enemies -= 1
+            elif rect_intersect(bullet_rect, enemy2_green_rect[j]):
+                SCREEN.blit(EXPLOSION_GREEN, enemy2_green_rect[j])
                 pygame.time.wait(20)
-                explosion_sound.set_volume(0.05)
-                explosion_sound.play()
+                EXPLOSION_SOUND.set_volume(0.05)
+                EXPLOSION_SOUND.play()
                 enemy2_green_rect[j].y = 600
-                BULLET_STATE = "Ready"
+                bullet_state = "Ready"
                 enemy2_green_hit[j] = True
                 score += 10
+                total_num_of_enemies -= 1
 
 
 def is_gameover():
-    global enemy_purple_hit, enemy1_cyan_hit, enemy2_cyan_hit, enemy1_green_hit
-    global enemy2_green_hit
-    for i in range(NUM_OF_ENEMIES):
-        if (enemy_purple_hit[i] is not True
-           or enemy1_cyan_hit[i] is not True
-           or enemy2_cyan_hit[i] is not True
-           or enemy1_green_hit[i] is not True
-           or enemy2_green_hit[i] is not True):
-            return False
-        else:
-            return True
-    # global score
-    # if score >= 900:
-    #     return True
-    # return False
+    global total_num_of_enemies
+    if total_num_of_enemies == 0:
+        return True
+    return False
 
 
 def is_enemy_hit_ship():
-    global enemy_purple_hit, enemy1_cyan_hit, enemy2_cyan_hit, enemy1_green_hit
-    global enemy2_green_rect
+    global enemy_purple_hit, enemy1_cyan_hit, enemy2_cyan_hit, \
+        enemy1_green_hit, enemy2_green_rect
     for i in range(NUM_OF_ENEMIES):
         if (enemy_purple_rect[i].y >= 410 and enemy_purple_hit[i] is not True
-           or enemy1_cyan_rect[i].y >= 410 and enemy1_cyan_hit[i] is not True
-           or enemy2_cyan_rect[i].y >= 410 and enemy2_cyan_hit[i] is not True
-           or enemy1_green_rect[i].y >= 410 and enemy1_green_hit[i] is not True
-           or enemy2_green_rect[i].y >= 410 and enemy2_green_hit[i] is not True):
+            or enemy1_cyan_rect[i].y >= 410 and enemy1_cyan_hit[i] is not True
+            or enemy2_cyan_rect[i].y >= 410 and enemy2_cyan_hit[i] is not True
+            or enemy1_green_rect[i].y >= 410 and enemy1_green_hit[i] is not
+            True or enemy2_green_rect[i].y >= 410 and enemy2_green_hit[i] is
+                not True):
             return True
     return False
 
 
 def bunker_collision():
     for k in range(3):
-        if rect_intersect(BULLET_RECT, bunkers[k]):
+        if rect_intersect(bullet_rect, BUNKERS[k]):
             pass
             # print("Collide")
 
@@ -368,7 +364,7 @@ def random_point_mystery(m_rect):
 
 
 def main():
-    global BULLET_Y, BULLET_STATE, BULLET_RECT, BULLET_X
+    global bullet_y, bullet_state, bullet_rect, bullet_x
     pygame.key.set_repeat(1, 10)
     background_sound = mixer.Sound(SOUND_PATH + "background.wav")
     background_sound.set_volume(0.5)
@@ -395,28 +391,28 @@ def main():
         bunker()
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and SHIP_RECT.x > 10:
-            SHIP_RECT.x -= SHIP_SPEED
-        if keys[pygame.K_RIGHT] and SHIP_RECT.x < 740:
-            SHIP_RECT.x += SHIP_SPEED
+        if keys[pygame.K_LEFT] and ship_rect.x > 10:
+            ship_rect.x -= SHIP_SPEED
+        if keys[pygame.K_RIGHT] and ship_rect.x < 740:
+            ship_rect.x += SHIP_SPEED
         if keys[pygame.K_SPACE]:
-            if BULLET_STATE == "Ready":
-                BULLET_Y = 540
+            if bullet_state == "Ready":
+                bullet_y = 540
                 bullet_sound = mixer.Sound(SOUND_PATH + "shoot.wav")
                 bullet_sound.set_volume(0.05)
                 bullet_sound.play()
-                BULLET_X = SHIP_RECT.x
-                bullet(BULLET_X, BULLET_Y)
-        SCREEN.blit(SHIP, SHIP_RECT)
+                bullet_x = ship_rect.x
+                bullet(bullet_x, bullet_y)
+        SCREEN.blit(SHIP, ship_rect)
 
         # Bullet movement
-        if BULLET_Y <= 0:
-            BULLET_Y = 540
-            BULLET_STATE = "Ready"
-        if BULLET_STATE == "Fire":
-            bullet(BULLET_X, BULLET_Y)
-            BULLET_Y -= BULLET_SPEED
-        BULLET_RECT = BULLET.get_rect(topleft=(BULLET_X + 23, BULLET_Y))
+        if bullet_y <= 0:
+            bullet_y = 540
+            bullet_state = "Ready"
+        if bullet_state == "Fire":
+            bullet(bullet_x, bullet_y)
+            bullet_y -= BULLET_SPEED
+        bullet_rect = BULLET.get_rect(topleft=(bullet_x + 23, bullet_y))
 
         enemies_collision()
         bunker_collision()
@@ -435,13 +431,6 @@ def main():
             SCREEN.blit(gameover_text, (250, 250))
             background_sound.stop()
             break
-
-        print(is_gameover())
-        print(enemy_purple_hit)
-        print(enemy1_cyan_hit)
-        print(enemy2_cyan_hit)
-        print(enemy1_green_hit)
-        print(enemy2_green_hit)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
